9598ec72bde41a425a7e429a6088ba70c8d1186a
MPanda <HamHwong@gmail.com>
2020-12-21 17:48:46 +0800

    fix API

--- homepage/src/api/index.js
+++ homepage/src/api/index.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-21 00:23:48
+ * @LastEditTime: 2020-12-21 13:18:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /homepage/src/api/index.js
+ */
 import request from '@/utils/request'
 export async function UploadImage(params){
   return request({
-    url: 'http://localhost:8100/v1/imageUpload',
+    url: 'http://localhost:8100/api/v1/imageUpload',
     method: 'post',
     data: params
   }) 
