725917756a5bd325c283261fe3ac1eedf11eb26b
Ham <hhuang30@dxc.com>
2021-03-29 22:41:02 +0800

    Change the color

--- src/components/TopNav/components/Lotties/index.vue
+++ src/components/TopNav/components/Lotties/index.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="warpper" >
-    <div @click="switchStatus" class="container" ref="container"></div>
+  <div class="warpper">
+    <div
+      @click="switchStatus"
+      class="container"
+      ref="container"
+    ></div>
   </div>
 </template>
 
 <script>
 import lottie from "lottie-web";
-import { replaceColor ,getColors} from 'lottie-colorify';
-import { onMounted, ref, reactive,watch} from "vue";
-import {inject} from 'vue'
+// import { replaceColor ,getColors} from 'lottie-colorify';
+import { onMounted, ref, reactive, watch } from "vue";
+import lottieApi from 'lottie-api'
+import { inject } from 'vue'
 import animation from "@/assets/Lotties/menu.json";
 export default {
   name: "Lotties",
-  setup(props, context) {
+  setup (props, context) {
     var container = ref(null);
     var lottieObj = ref(null);
     var isShownGB = inject("isShownBG");
     var options = reactive({
       status: "fold",
     });
-    onMounted(function() {
+    onMounted(function () {
       lottieObj = initAnimation(animation)
     });
-    function initAnimation(animation){  
+    function initAnimation (animation) {
       return lottie.loadAnimation({
         container: container.value, // the dom element that will contain the animation
         renderer: "svg",
@@ -31,42 +36,59 @@ export default {
         animationData: animation //animation, // the path to the animation json
       });
     }
-    // watch(isShownGB,(val,oldval)=>{
-    //   // lottieObj = initAnimation(replaceColor([51, 51, 51],[255,255,255], animation))
-    //   // console.log(getColors(lottieObj))
-    //   // animation.animationData = replaceColor([51, 51, 51],[255,255,255], animation)
-    //   //replaceColor('#333333','#ffffff',animation)
-    //   lottieObj.destroy()
-    //     if(val){
-    //       lottieObj=initAnimation(replaceColor('#333333','#ffffff',animation))
-    //     }else{ 
-    //       lottieObj=initAnimation(animation)
-    //     }
-    // })
-    function goToAndStop(value, isFrame) {
+    watch(isShownGB, (val, oldval) => {
+      if (val) {
+        changeColor([1, 1, 1, 1])
+      } else {
+        changeColor([0, 0, 0, 1])
+      }
+    })
+    function changeColor ([r, g, b, a]) {
+      const api = lottieApi.createAnimationApi(lottieObj)
+      var arr = ['Upper', 'Center', 'Buttom']
+      arr.map(i => {
+        const element = api.getKeyPath(`Contents,${i},Fill 1,Color`)
+        api.addValueCallback(element, function () {
+          return [r, g, b, a];
+        });
+      })
+      var curFrame = 0
+      if (options.status == "fold") {
+        console.log('Fold1')
+        curFrame = 0 + Math.random() * 0.00001
+        // lottieObj.goToAndStop(curFrame,false) 
+        lottieObj.playSegments([0, 1], true);
+      } else {
+        console.log('UnFold1')
+        curFrame = 70 + Math.random() * 0.00001
+        // lottieObj.goToAndStop(curFrame,false) 
+        lottieObj.playSegments([69, 70], true);
+      }
+    }
+    function goToAndStop (value, isFrame) {
       lottieObj.goToAndStop(value, isFrame);
     }
-    function goToAndPlay(value, isFrame) {
+    function goToAndPlay (value, isFrame) {
       lottieObj.goToAndPlay(value, isFrame);
     }
-    function fold() { 
+    function fold () {
       context.emit('menu:fold')
-      options.status = 'fold' 
-      lottieObj.playSegments([70, 140], true);
+      options.status = 'fold'
+      lottieObj.playSegments([70, 140], false);
     }
-    function unfold() { 
+    function unfold () {
       context.emit('menu:unfold')
-      options.status = 'unfold' 
-      lottieObj.playSegments([0, 70], true);
+      options.status = 'unfold'
+      lottieObj.playSegments([0, 70], false);
     }
-    function stop() {
+    function stop () {
       fold();
     }
-    function play() {
+    function play () {
       unfold();
     }
-    function switchStatus() {      
-      if (options.status == "fold") { 
+    function switchStatus () {
+      if (options.status == "fold") {
         unfold();
       } else {
         fold();
@@ -76,7 +98,7 @@ export default {
     return {
       container,
       stop,
-      play, 
+      play,
       lottieObj,
       options,
       switchStatus,
@@ -88,8 +110,8 @@ export default {
 
 <style lang="scss" scoped>
 .warpper {
-  background-color:rgba(255,255,255,0.3);
-  border-radius:0 0 5px 0;
+  // background-color:rgba(255,255,255,0.3);
+  // border-radius:0 0 5px 0;
   overflow: hidden;
   .container {
     width: 40px;
@@ -97,5 +119,4 @@ export default {
     cursor: pointer;
   }
 }
-
 </style>
