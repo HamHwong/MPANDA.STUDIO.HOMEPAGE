af3967334ddca4563f632ed49246488e976dbb56
MPanda <HamHwong@gmail.com>
2021-04-08 17:44:22 +0800

    fix

--- src/components/Game/lib/sprints/Imodels/Characters.js
+++ src/components/Game/lib/sprints/Imodels/Characters.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-08 15:13:57
- * @LastEditTime: 2021-04-08 16:38:33
+ * @LastEditTime: 2021-04-08 17:43:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /MPANDA.STUDIO.HOMEPAGE/src/components/Game/lib/sprints/character.js
@@ -18,13 +18,13 @@ export class Characters extends Instance{
     this.type = 'characters'
   }
   async load(){ 
-    const arr = []
-    const frame1 = new frame()
-    frame1.img = await load(require('../../../static/characters/char1/init/char1-init-1.png'))
-    const frame2 = new frame()
-    frame2.img = await load(require('../../../static/characters/char1/init/char1-init-2.png'))
-    arr.push(frame1)
-    arr.push(frame2)
-    this.addStatusFrames('init', arr) 
+    // const arr = []
+    // const frame1 = new frame()
+    // frame1.img = await load(require('../../../static/characters/char1/init/char1-init-1.png'))
+    // const frame2 = new frame()
+    // frame2.img = await load(require('../../../static/characters/char1/init/char1-init-2.png'))
+    // arr.push(frame1)
+    // arr.push(frame2)
+    // this.addStatusFrames('init', arr) 
   }
 }
