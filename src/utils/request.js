cca6e14233b802df69c2e7edc7029cb9bd98266e
MPanda <HamHwong@gmail.com>
2021-04-20 15:29:19 +0800

    fix some thong

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-21 00:23:48
- * @LastEditTime: 2021-04-19 09:23:20
+ * @LastEditTime: 2021-04-20 15:28:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /homepage/src/utils/request.js
@@ -9,7 +9,7 @@
 import axios from 'axios';
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
 const service = axios.create({
-  baseURL:'http://localhost:8100',
+  baseURL:process.env.VUE_APP_BASE_API,
   // baseURL:'https://api.mpanda.studio/',
   // 超时
   timeout: 10000
