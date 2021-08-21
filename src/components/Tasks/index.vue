6b47559fae38dcc6c511e029d7dfbef63c1016b0
MPanda <HamHwong@gmail.com>
2021-08-21 16:13:41 +0800

    fix something

--- src/components/Tasks/index.vue
+++ src/components/Tasks/index.vue
@@ -1,56 +1,69 @@
 <template>
   <div class="todolist_container">
-    <div 
+    <div
       v-for="i in list"
       :key="i._id"
       class="todolist_row"
     >
-      <el-checkbox
-        :label="i.content"
-      />  
-      <small> 
-        <el-divider direction="vertical" /> 
-        {{ dateformat(i.createDate, 'yyyy-mm-dd HH:MM') }}  
-      </small> 
+      <div
+        v-tooltips
+        class="title"
+      >
+        <span> {{ i.content }}</span>
+      </div>
+      <small class="date">
+        <el-divider direction="vertical" />
+        {{ dateformat(i.createDate, 'yyyy-mm-dd HH:MM') }}
+      </small>
     </div>
   </div>
 </template>
 
 <script>
-import {Tasks} from '@/api'
+import { Tasks } from '@/api'
 import dateformat from 'dateformat'
 import { onMounted, reactive } from '@vue/runtime-core'
-  export default {
-    setup(){
-      const list = reactive([])
-      onMounted(async ()=>{
-        const {IsSuccess,Message,Data} = await Tasks.List()
-        if(IsSuccess){ 
-          Data.map(i=>list.push(i))
-        }else{
+export default {
+  setup () {
+    const list = reactive([])
+    onMounted(async () => {
+      const { IsSuccess, Message, Data } = await Tasks.List()
+      if (IsSuccess) {
+        Data.map(i => list.push(i))
+      } else {
 
-        }
-        console.log(list)
-      })
-      return {
-        list,
-        dateformat
       }
+      console.log(list)
+    })
+    return {
+      list,
+      dateformat
     }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.todolist_container{
+.todolist_container {
   text-align: left;
-  &:deep(.el-checkbox){
-    color:#fff;
+  &:deep(.el-checkbox) {
+    color: #fff;
   }
-  .todolist_row{
+  .todolist_row {
     display: flex;
-    justify-content: space-between;
     align-items: center;
+    justify-content: space-between;
     margin: 5px 0;
+    .title {
+      flex: 2;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .date {
+      // justify-self: flex-end;
+      // flex: 1;
+    }
   }
 }
 </style>
